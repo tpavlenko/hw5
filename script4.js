b721c9c02d8d9d085ed719ec4e3358d868bb360d
Tetiana Pavlenko <72665178+tpavlenko@users.noreply.github.com>
2021-03-03 18:49:10 +0200

    Add files via upload

--- script4.js
+++ script4.js
@@ -2,18 +2,26 @@
 
 //  'func("hello world", ['l', 'd'])' вернет нам "heo wor"
 
-var str = "hello world";
 
-// function getCutSymbols(str, arrDeleted) {
-var arrString = [];
-arrString = str.split("");
-var arrDeleted = ['l', 'd'];
-for (var i = 0; i < arrDeleted.length; i++) {
-	var symbolDeleted = arrDeleted.indexOf(arrDeleted[i]);
-	if (arrString[i] != symbolDeleted[i]) {
-		var spliced = arrString.splice(arrString[i]);
-		return spliced;
+
+function getCutSymbols(str, arrDeleted) {
+	var arrString = [];
+	arrString = str.split("");
+
+	for (var i = 0; i < arrDeleted.length; i++) {
+		var symbolDeleted = arrString.indexOf(arrDeleted[i]);
+
+		if (symbolDeleted >= 0) {
+			arrString.splice(symbolDeleted, 1);
+			i--;
+		}
 	}
+
+	var resultString = arrString.join('');
+
+	return resultString;
 }
+
 var result = getCutSymbols("hello world", ['l', 'd']);
+
 console.log(result);
--- script5.js
+++ script5.js
@@ -5,16 +5,34 @@ var input = [1, 2, 3, 4, 5, 6];
 function isEven(x) { return x % 2 == 0; } // проверяет на четность
 console.log(filter(input, isEven)); // [2, 4, 6]*/
 
+// var input = [1, 2, 3, 4, 5, 6];
+
+// function filter(input, callback) {
+// 	for (var x = 0, size = input.length; x < size; x++) {
+// 		return callback(input[x]);
+// 	}
+// }
+
+
+
 var input = [1, 2, 3, 4, 5, 6];
 
 function filter(input, callback) {
-	for (var x = 0, size = input.length; x < size; x++) {
-		return callback(input[x]);
+	var arr = [];
+
+	for (var i = 0, size = input.length; i < size; i++) {
+		var x = input[i];
+		if (callback(x) === true) {
+			arr.push(x);
+		}
 	}
+	return arr;
 }
 
 function isEven(x) {
 	return x % 2 == 0;
 }
 
-console.log(filter(input, isEven));
+var result = filter(input, isEven);
+
+console.log(result);
